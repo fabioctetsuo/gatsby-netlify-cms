aae5105d364cbdc8685f9a125c97a374566222ce
Fabio Tetsuo <fabioctetsuo@gmail.com>
2021-01-26 16:55:02 -0300

    feat: add gatsby blog first config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     title: "gatsby-netlify-cms",
   },
   plugins: [
-    "gatsby-plugin-netlify-cms",
     "gatsby-plugin-styled-components",
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-remark",
@@ -15,5 +14,42 @@ module.exports = {
       },
       __key: "pages",
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "uploads",
+        path: "./static/assets/img/",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: "./posts/",
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-relative-images`,
+            options: {
+              name: "uploads",
+            }, // Esse plugin ajuda o markdown a buscar as imagens dentro da pasta de static, sem ele, ele não consegue pegar a imagem com o caminho relativo
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    "gatsby-plugin-netlify-cms",
   ],
 };
